f7dfe15e7ec5fd2cf6490583f31d65c14e002753
mamoruitoi <mamoruitoi@icloud.com>
2020-02-17 22:33:49 +0900

    first commit

---
+++ twitter.py
@@ -0,0 +1,21 @@
+def main():
+    editor = input("What editor do you like?")
+    type = input("What is it?")
+    template = """
+
+
+                           ₍₍⁽⁽""" + editor + """₎₎⁾⁾
+
+    見て！""" + editor + """が踊っているよ
+    かわいいね
+
+    ㅤ
+
+                               """ + editor + """
+
+    みんなが別のエディタばかり使っているいるので、""" + editor + """は踊るのをやめてしまいました お前らのせいです!
+    あ〜あ
+    """
+    return template
+
+print(main())
