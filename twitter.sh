edab1c7b2c6252a9853c7c516a2ae52c11772025
laminne <gtattoda4@gmail.com>
2020-02-19 19:45:51 +0900

    shellecript版を追加した

---
+++ twitter.sh
@@ -0,0 +1,11 @@
+
+#! bin/bash
+
+echo What editor do you like?:
+
+read editor
+echo "\n"
+echo "\n"
+echo ₍₍⁽⁽  $editor  ₎₎⁾⁾
+
+echo "\n\n見て！$editor が踊っているよ\nかわいいね\n \n$editor\n\nみんなが別のエディタばかり使っているので、\n$editor は踊るのをやめてしまいました お前らのせいです!\nあ〜あ"
